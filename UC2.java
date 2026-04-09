433286022f9f2551b2c2e4b72a983c14a7c0108d
yuktibazari <yuktibazari@gmail.com>
2026-04-09 23:45:21 +0530

    UC2 added to HelloApp

---
+++ UC2.java
@@ -0,0 +1,11 @@
+public class UC2 {
+    public static void main(String[] args) {
+
+        if (args.length > 0) {
+            System.out.println("Hello, " + args[0] + "!");
+        } else {
+            System.out.println("Hello, World!");
+        }
+
+    }
+}
